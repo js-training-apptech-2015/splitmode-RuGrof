7656fd5e26ef3926769c391d3cc4a1e1341d6bf4
RuGrof <RuGrof@gmail.com>
2015-11-24 20:30:14 +0500

    Final splitmode 1

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 var TURN_FIRST = 1;
 var TURN_SECOND = 2;
+var STATE_GAME = 0;
+var STATE_END_GAME = 1;
 
 function GameTicTac() {
     this.gameField = [[0, 0, 0],
@@ -8,6 +10,7 @@ function GameTicTac() {
     this.freeField = 9;
     this.playerFirstScore = 0;
     this.playerSecondScore = 0;
+    this.state = STATE_GAME;
     this.turn = TURN_FIRST;
 };
 
@@ -18,6 +21,7 @@ GameTicTac.prototype.newGame = function () {
     this.freeField = 9;
     this.playerFirstScore = 0;
     this.playerSecondScore = 0;
+    this.state = STATE_GAME;
     this.turn = TURN_FIRST;
 };
 
@@ -42,6 +46,7 @@ GameTicTac.prototype.continueGame = function (turnFirst) {
                           [0, 0, 0],
                           [0, 0, 0]];
         this.freeField = 9;
+        this.state = STATE_GAME;
         this.turn = turnFirst;
 
     } catch (err) {
@@ -50,7 +55,7 @@ GameTicTac.prototype.continueGame = function (turnFirst) {
 }
 
 /**
- * changeTurn change TURN_FIRST to TURN_SECOND and vice versa
+ * changeTurn change TURN_FIRST to TURN_SECOND and vice versa, and changeFreeField()
  * @property changeTurn
  */
 GameTicTac.prototype.changeTurn = function () {
@@ -59,6 +64,7 @@ GameTicTac.prototype.changeTurn = function () {
     } else {
         this.turn = TURN_FIRST;
     }
+
 }
 
 /**
@@ -74,6 +80,7 @@ GameTicTac.prototype.changeFreeField = function () {
 GameTicTac.prototype.checkVictory = function () {
     var result;
     var i;
+    this.changeFreeField();
     //Diagonal
     if (this.gameField[0][0] === this.gameField[1][1] &&
         this.gameField[0][0] === this.gameField[2][2] &&
@@ -110,6 +117,7 @@ GameTicTac.prototype.checkVictory = function () {
             result = -1;
         }
     }
+    console.log(result);
     return result;
 }
 
@@ -118,14 +126,14 @@ GameTicTac.prototype.checkVictory = function () {
  * Adds score to winner, win - 2 points, draw - 1 point
  * @param {TURN_FIRST || TURN_SECOND || 0} winner 0-draw
  */
-GameTicTac.prototype.winScore = function(winner){
-    if(winner === 0){
+GameTicTac.prototype.winScore = function (winner) {
+    if (winner === 0) {
         this.playerFirstScore += 1;
         this.playerSecondScore += 1;
-    }else if( winner === TURN_FIRST){
+    } else if (winner === TURN_FIRST) {
         this.playerFirstScore += 2;
-    }else{
-        this.playerSecondScore +=2;
+    } else {
+        this.playerSecondScore += 2;
     }
 }
 
@@ -135,17 +143,142 @@ GameTicTac.prototype.winScore = function(winner){
  * @param {number} x      0<=x<=3 coordinate cell
  * @param {number} y      0<=y<=3 coordinate cell
  */
-GameTicTac.prototype.playerTurn = function(player,x,y){
+GameTicTac.prototype.playerTurn = function (x, y) {
+
     var result; // 1 succesfull Turn
-    if(this.gameField[y][x] === 0){
-        this.gameField[y][x] = player;
+    if (this.gameField[y][x] === 0) {
+        this.gameField[y][x] = this.turn;
         result = 1;
-    }else{
+    } else {
+        result = 0;
+    }
+    return result;
+}
+
+/**
+ * Description for gameTurn
+ * @private
+ * @method gameTurn
+ * @param {Object} x
+ * @param {Object} y
+ * @return {Number} 1 - successful, 0 - no change, 2 - end game
+ */
+GameTicTac.prototype.gameTurn = function (x, y) {
+    var result;
+    var chekVictory;
+    if (this.playerTurn(x, y) > 0) {
+        result = 1;
+        chekVictory = this.checkVictory();
+        if (chekVictory < 0) {
+            //  game not ending
+            this.changeTurn();
+        } else {
+            // game end
+            result = 2;
+            console.log('end game')
+            this.state = STATE_END_GAME;
+            this.winScore(chekVictory);
+        }
+    } else {
         result = 0;
     }
 }
 
 
-function GameLayout (){
-    this.menu =
+
+function GameLayout(gameTicTac) {
+    this.game = gameTicTac;
+    this.newGame = $('.top-menu__new-game');
+    this.mode = $('.top-menu__mode');
+    this.playerFirstScoreboard = $('.scoreboard__player1');
+    this.playerSecondScoreboard = $('.scoreboard__player2');
+    this.playerFirstScore = $('#playerFirstScore');
+    this.playerSecondScore = $('#playerSecondScore');
+    this.dialogGameOver = $('#dialog');
+    this.turn = 'X';
+    // y=Math.floor(index/3) x=index%3
+    this.gameField = function (x, y) {
+        return $('.game__cell')[3 * y + x];
+    }
 }
+
+
+/**
+ * Render Game in layout
+ */
+GameLayout.prototype.render = function () {
+
+    var fillTic = 'X';
+    var fillTac = 'O';
+    var i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (this.game.gameField[i][j] === TURN_FIRST) {
+                $(this.gameField(j, i)).text(fillTic);
+            } else if (this.game.gameField[i][j] === TURN_SECOND) {
+                $(this.gameField(j, i)).text(fillTac);
+            } else {
+                $(this.gameField(j, i)).text('');
+            }
+        }
+    }
+
+    if (this.game.turn === TURN_FIRST) {
+        this.playerFirstScoreboard.addClass('scoreboard__hightlight');
+        this.playerSecondScoreboard.removeClass('scoreboard__hightlight');
+    } else {
+        this.playerSecondScoreboard.addClass('scoreboard__hightlight');
+        this.playerFirstScoreboard.removeClass('scoreboard__hightlight');
+    }
+
+    this.playerFirstScore.text(this.game.playerFirstScore);
+    this.playerSecondScore.text(this.game.playerSecondScore);
+    if (this.game.state === STATE_END_GAME) {
+        this.dialogGameOver.dialog('open');
+    }
+}
+
+
+GameLayout.prototype.playerTurn = function (x, y) {
+
+}
+
+$(function () {
+    var game = new GameTicTac();
+    var layout = new GameLayout(game);
+    layout.newGame.on('click', function () {
+        game.newGame();
+        layout.render();
+    });
+
+    $('.game__cell').each(function () {
+        $(this).on('click', function () {
+            game.gameTurn($(this).index() % 3, Math.floor($(this).index() / 3));
+            layout.render();
+            console.log($(this).index());
+        });
+    });
+
+    $("#dialog").dialog({
+        autoOpen: false,
+        width: 400,
+        buttons: [
+            {
+                text: "New game",
+                click: function () {
+                    game.newGame();
+                    layout.render();
+                    $(this).dialog("close");
+                }
+		},
+            {
+                text: "Continue game",
+                click: function () {
+                    game.continueGame();
+                    layout.render();
+                    $(this).dialog("close");
+                }
+		}
+	]
+    });
+});
